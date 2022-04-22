e1c4f90d054fbf0e3ccbf742947b8efa0e96bdfa
Nilotpal <kutan16@gmail.com>
2022-04-22 21:30:21 +0530

    implemented Db configuration using property file values

--- student-demo-api/src/main/java/nilotpal/jaxrs/init/MyApplicationBinder.java
+++ student-demo-api/src/main/java/nilotpal/jaxrs/init/MyApplicationBinder.java
@@ -3,6 +3,7 @@ package nilotpal.jaxrs.init;
 import nilotpal.config.DatasourceConfig;
 import nilotpal.config.PropertyConfig;
 import nilotpal.service.*;
+import nilotpal.util.DBUtil;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +11,7 @@ import org.slf4j.LoggerFactory;
 import javax.inject.Singleton;
 
 /**
- *Custom Binder to bind classes as well as implemented interfaces to be available for injection
+ * Custom Binder to bind classes as well as implemented interfaces to be available for injection
  */
 public class MyApplicationBinder extends AbstractBinder {
     private final static Logger log = LoggerFactory.getLogger(MyApplicationBinder.class);
@@ -21,13 +22,18 @@ public class MyApplicationBinder extends AbstractBinder {
     @Override
     protected void configure() {
         log.info("Creating my application binder");
-        bindAsContract(DatasourceConfig.class).in(Singleton.class);
+//        bindAsContract(DatasourceConfig.class).in(Singleton.class);
         bindAsContract(CommonDataService.class);
         bind(StudentService.class).named("studentService").to(ServiceInterface.class).in(Singleton.class);
         bind(EmployeeService.class).named("employeeService").to(ServiceInterface.class).in(Singleton.class);
         bind(UserServiceImpl.class).named("userService").to(UserService.class).in(Singleton.class);
         bindAsContract(ServiceInterface.class);
         bindAsContract(UserService.class);
-        bind(PropertyConfig.class).to(DatasourceConfig.class).in(Singleton.class);
+        bindAsContract(PropertyConfig.class).in(Singleton.class);
+        bindAsContract(DatasourceConfig.class).in(Singleton.class);
+        bindAsContract(DBUtil.class).in(Singleton.class);
+        bindAsContract(UserServiceImpl.class).in(Singleton.class);
+        bindAsContract(StudentService.class).in(Singleton.class);
+        bindAsContract(EmployeeService.class).in(Singleton.class);
     }
 }
--- student-demo-api/src/main/java/nilotpal/jaxrs/init/MyApplicationConfig.java
+++ student-demo-api/src/main/java/nilotpal/jaxrs/init/MyApplicationConfig.java
@@ -1,7 +1,11 @@
 package nilotpal.jaxrs.init;
 
+import nilotpal.config.DatasourceConfig;
 import nilotpal.jaxrs.authentication.AuthenticationFilter;
 import nilotpal.jaxrs.resource.StudentResource;
+import nilotpal.service.EmployeeService;
+import nilotpal.service.StudentService;
+import nilotpal.service.UserServiceImpl;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,5 +24,9 @@ public class MyApplicationConfig extends ResourceConfig {
         register(new MyApplicationBinder());
         register(StudentResource.class);
         register(AuthenticationFilter.class);
+//        register(DatasourceConfig.class);
+//        register(EmployeeService.class);
+//        register(StudentService.class);
+//        register(UserServiceImpl.class);
     }
 }
